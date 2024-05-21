3d4b5f1f653ece010bd80c152fa847eda3db0e35
Bobbyshu <shu.yun@northeastern.edu>
2024-05-20 22:38:39 -0400

    feat(): delete todo by id

--- src/main/java/com/example/rest/webservices/restfulwebservices/todo/TodoResource.java
+++ src/main/java/com/example/rest/webservices/restfulwebservices/todo/TodoResource.java
@@ -1,5 +1,7 @@
 package com.example.rest.webservices.restfulwebservices.todo;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,5 +25,11 @@ public class TodoResource {
   @GetMapping("/users/{username}/todos/{id}")
   public Todo retrieveTodo(@PathVariable String username, @PathVariable int id) {
     return todoService.findById(id);
+  }
+
+  @DeleteMapping("/users/{username}/todos/{id}")
+  public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable int id) {
+    todoService.deleteById(id);
+    return ResponseEntity.noContent().build();
   }
 }
